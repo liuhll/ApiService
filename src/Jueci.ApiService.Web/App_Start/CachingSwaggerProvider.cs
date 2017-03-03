151000ffbdb8b383d69df12c1fd5c7859d998ea0
Liuhongliang <1029765111@qq.com>
2017-03-03 14:50:30 +0800

    集成Swagger框架,自动生成Api文档

---
+++ src/Jueci.ApiService.Web/App_Start/CachingSwaggerProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Swashbuckle.Swagger;
+
+namespace Jueci.ApiService.Web
+{
+    public class CachingSwaggerProvider : ISwaggerProvider
+    {
+        private readonly ISwaggerProvider _swaggerProvider;
+
+        public CachingSwaggerProvider(ISwaggerProvider defaultProvider)
+        {
+            _swaggerProvider = defaultProvider;
+        }
+
+        public SwaggerDocument GetSwagger(string rootUrl, string apiVersion)
+        {
+            var sd = _swaggerProvider.GetSwagger(rootUrl, apiVersion);
+
+            var rmPaths = new List<string>();
+
+            foreach (var path in sd.paths)
+            {
+                if (path.Key.Contains("Abp") || path.Key.Contains("TypeScript"))
+                {
+                    rmPaths.Add(path.Key);
+                }
+            }
+            foreach (var rmpath in rmPaths)
+            {
+                if (sd.paths.ContainsKey(rmpath))
+                {
+                    sd.paths.Remove(rmpath);
+                }
+
+            }
+
+            return sd;
+        }
+    }
+}
---
+++ src/Jueci.ApiService.Web/Commons/Enums/CommentsType.cs
@@ -0,0 +1,9 @@
+﻿namespace Jueci.ApiService.Web.Commons.Enums
+{
+    public enum CommentsType
+    {
+        App = 1,
+        WebApi = 2,
+        Model = 3
+    }
+}
