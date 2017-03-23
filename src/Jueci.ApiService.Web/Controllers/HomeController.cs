8ec71f9c087973ea914f3a42a913ba4371be68e5
Liuhongliang <1029765111@qq.com>
2017-03-23 10:14:42 +0800

    系统启动后，重定向到API文档

--- src/Jueci.ApiService.Web/Controllers/HomeController.cs
+++ src/Jueci.ApiService.Web/Controllers/HomeController.cs
@@ -5,8 +5,9 @@ namespace Jueci.ApiService.Web.Controllers
     public class HomeController : ApiServiceControllerBase
     {
         public ActionResult Index()
-        { 
-            return View("~/App/Main/views/layout/layout.cshtml"); //Layout of the angular application.
+        {
+            // return View("~/App/Main/views/layout/layout.cshtml"); //Layout of the angular application.
+            return Redirect(Request.Url.OriginalString + "swagger/ui/index");
         }
 	}
 }
